b88ce73804fdc5c0b367c167473398e8397dadd2
a.t.janse@students.uu.nl <a.t.janse@students.uu.nl>
2021-12-07 04:10:49 +0100

    Better explained my solution in the comments

--- 6a.py
+++ 6a.py
@@ -9,14 +9,15 @@ days = 80
 
 """
 I realized that you can just swap the cycles around. I first cycled
-over the 7-day birthing cycle, but then you would get
-
-    fishes[(day + 9) % 7] = fishes[(day + 2) % 7]
-
-which fails, because the growth time would be only 2 days this way.
-I circumvented this problem by making a second 7-cycle list, storing
-the offsprings and introducing the offsprings into the "adult pool"
-at the beginning of the day.
+over the 7-day birthing cycle, but then you would get something like
+(t + 9) % 7, which is equivalent to (t + 2) % 7. This would mean
+that you would encounter the fishes at t + 2 and you wouldn't be able
+to easily distiguish between the mature fish and the younglings.
+I circumvented this problem by making a seperate 9-cycle, storing
+the younglings. At each day, i would join the younglings with the
+adults: like having the fish mature in a seperate pool or something.
+Then I would "take the eggs" of the mature fish and place them in the
+empty maturing pool and store them away for 9 days.
 
 This new solution is much more elegant. You can just cycle over the
 growth cycle; the current day fishes "lay their eggs", so that they
@@ -24,6 +25,6 @@ are fully grown in the next cycle. Meanwhile, we add the fishes to
 (day + 7) % 9, because on that day they will lay eggs again.
 """
 
-for day in range(days): fishes[(day + 7) % 9] += fishes[day % 9]
+for t in range(days): fishes[(t + 7) % 9] += fishes[t % 9]
 
 print(sum(fishes))
--- 6b.py
+++ 6b.py
@@ -8,6 +8,6 @@ for i in map(int, f.readline().split(',')):
 days = 256
 
 # Explanation in 6a.py
-for day in range(days): fishes[(day + 7) % 9] += fishes[day % 9]
+for t in range(days): fishes[(t + 7) % 9] += fishes[t % 9]
 
 print(sum(fishes))
