6390f2e81df19f3151c96ecaa006e5d392b8b6d6
a.t.janse@students.uu.nl <a.t.janse@students.uu.nl>
2021-12-07 04:41:16 +0100

    Added loads of comments

--- 3b.py
+++ 3b.py
@@ -1,50 +1,55 @@
+from typing import List
+
 f = open('./input/3.txt')
 
-numbers = []
+bit_strings = []
 
-line = f.readline()
-bits = len(line) - 1
+bit_string = f.readline()
+bits = len(bit_string) - 1 # again, fuck \n
 
-while line != '':
-    numbers.append(line)
+while bit_string != '':
+    bit_strings.append(bit_string)
     
-    line = f.readline()
+    bit_string = f.readline()
 
-def filterList(numberList, bit, most):
+def filterList(bit_strings: List[str], bit_position: int, most: bool) -> List[str]:
+    """
+    :param bit_strings: the list of bits strings
+    :param bit_position: the position of the bit that needs to be counted
+    :param most: whether we are looking for the most or least common bit
+    """
     count = 0
 
-    for number in numberList:
-        if number[bit] == '1': count += 1
+    for bit_string in bit_strings:
+        if bit_string[bit_position] == '1': count += 1
 
-    oneIsMost = count >= len(numberList) / 2
+    oneIsMost = count >= len(bit_strings) / 2
 
     value = '1' if (most and oneIsMost) or (not most and not oneIsMost) else '0'
     
-    return list(filter(lambda number: number[bit] == value, numberList))
+    return list(filter(lambda bit_string: bit_string[bit_position] == value, bit_strings))
 
-oxygenList = numbers.copy()
-co2List = numbers.copy()
+    # end function
 
-b = 0
+# Start with looking for the oxygen level
+oxygenList = bit_strings.copy()
+bit_position = 0
 
 while len(oxygenList) > 1:
-    oxygenList = filterList(oxygenList, b, True)
-    b += 1
+    # while there are still items in the list, keep filtering
+    oxygenList = filterList(oxygenList, bit_position, True)
+    bit_position += 1
 
-b = 0
+# Next, the co2 level
+co2List = bit_strings.copy()
+bit_position = 0
 
 while len(co2List) > 1:
-    co2List = filterList(co2List, b, False)
-    b += 1
+    co2List = filterList(co2List, bit_position, False)
+    bit_position += 1
 
-def bitsToInt(s):
-    result = 0
-    for b in range(bits):
-        result *= 2
-        result += int(s[b])
-    return result
-
-oxygen = bitsToInt(oxygenList[0])
-co2 = bitsToInt(co2List[0])
+# Convert the single left bit string to their numbers
+oxygen = int(oxygenList[0], 2)
+co2 = int(co2List[0], 2)
 
 print(oxygen * co2)
